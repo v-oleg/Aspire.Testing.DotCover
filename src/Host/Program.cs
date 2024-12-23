89be4e7e54eb267949471bda1d18d595cebb2062
Oleg Velb <82014227+v-oleg@users.noreply.github.com>
2024-12-23 16:45:05 -0600

    Initial commit

---
+++ src/Host/Program.cs
@@ -0,0 +1,3 @@
+var builder = DistributedApplication.CreateBuilder(args);
+builder.AddProject<Projects.Api>("api");
+builder.Build().Run();
---
+++ src/Tests/IntegrationTest1.cs
@@ -0,0 +1,28 @@
+namespace Tests.Tests;
+
+public class IntegrationTest1
+{
+    [Fact]
+    public async Task GetWebResourceRootReturnsOkStatusCode()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.Api>();
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        {
+            clientBuilder.AddStandardResilienceHandler();
+        });
+        // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
+    
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("api");
+        await resourceNotificationService.WaitForResourceAsync("api", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        var response = await httpClient.GetAsync("WeatherForecast");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
